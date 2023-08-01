2c63b787fe25b830f4b968b733ad7b068bbce878
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-08-02 00:49:43 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stoptBtn.addEventListener('click', onStopClick);
+
+let timerId;
+
+stoptBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartClick() {
+  timerId = setInterval(() => {
+    randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+}
